ab511caabec9f4532a5d967b2262d2364aae1704
mpv <193361506+mp-v99@users.noreply.github.com>
2025-02-22 23:40:11 -0600

    Write Pseudocode of round function

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+/* 
+PSEUDOCODE(Yes, after I actually wrote some code already):
+
+LOG Welcome Message for the user
+
+DECLARE Score variables
+
+DECLARE Computer Choice Function
+    DECLARE randomNumber that will return 1, 2 or 3
+    DECLARE undefined computerChoice variable
+    IF randomNumber EQUALS 1 
+        computerChoice EQUALS 'Rock'
+    ELSE IF randomNumber EQUALS 2
+        computerChoice EQUALS 'Paper'
+    ELSE
+        computerChoice EQUALS 'Scissors'
+
+DECLARE Human Choice Funciton 
+    DECLARE humanChoice prompt variable and turn into Upper Case
+    IF humanChoice EQUALS ROCK
+        RETURN humanChoice EQUALS 'Rock'
+    ELSE IF humanChoice EQUALS PAPER
+        RETURN humanChoice EQUAL 'Paper'
+    ELSE IF humanChoice EQUALS SCISSORS
+        RETURN humanChoice EQUALS 'Scissors'
+    ELSE
+        RETURN Error Message
+
+DECLARE Round Function
+    TAKE humanChoice and computerChoice parameters
+    IF humanChoice EQUALS computerChoice
+        RETURN "It's a tie!"
+        
+    ELSE IF humanChoice EQUALS Rock && computerChoice EQUALS Paper OR
+    humanChoice EQUALS Paper && computerChoice EQUALS Scissors OR
+    humanChoice EQUALS Scissors && computerChoice EQUALS Rock 
+
+        RETURN 'AI wins, sorry human. You played humanChoice and the machine played computerChoice'
+    ELSE 
+        RETURN 'Humanity has won, for now. You played humanChoice and the machine played computerChoice'
+*/
+
+
+
+
+
 console.log("Hello, and welcome to the epic ROCK-PAPER-SCISSORS Game!!!")
 
 let userScore = 0;
@@ -26,8 +72,11 @@ function getHumanChoice() {
         humanChoice = 'Rock';
     } else if (humanChoice === 'PAPER') {
         humanChoice = 'Paper';
-    } else {
+    } else if (humanChoice === 'SCISSORS' || humanChoice === 'SCISSOR') {
         humanChoice = 'Scissors';
+    }
+    else {
+        alert("Are you sure you wrote rock or paper or scissors?")
     }
     console.log(humanChoice)
 }
